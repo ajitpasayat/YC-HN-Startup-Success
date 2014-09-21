577e0bcbf0ee6dde9e1efe2f24832d8207dcbadf
Andrew Zaldivar <andrew.zaldivar@gmail.com>
2014-09-21 09:53:11 -0700

    Trying out Flask/Jinja2 to make MySQL queriers

--- app/views.py
+++ app/views.py
@@ -4,32 +4,23 @@ import pymysql as mdb
 
 db = mdb.connect(user="root", host="localhost", db="startupsuccess_db", charset='utf8')
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
-
-# This lets me now access the database and print it onto an HTML page. Cool.
-@app.route('/db')
-def startups_page():
-	with db: 
-		cur = db.cursor()
-		cur.execute("SELECT company FROM startupdata LIMIT 15;")
-		query_results = cur.fetchall()
-	startups = ""
-	for result in query_results:
-		startups += result[0]
-		startups += "<br>"
-	return startups
-
 @app.route('/startupdb')
 def startups_page_fancy():
-	with db:
-		cur = db.cursor()
-		cur.execute("SELECT company, sentiment FROM startupdata LIMIT 15;")
-		query_results = cur.fetchall()
-	startups = []
-	for result in query_results:
-		startups.append(dict(company=result[0], sentiment=result[1]))
-	return render_template('startups.html', startups=startups)
+    with db:
+        cura = db.cursor()
+        cura.execute("SELECT company, sentiment FROM startupdata LIMIT 15;")
+        query_results_a = cura.fetchall()
 
+        curb = db.cursor()
+        curb.execute("SELECT totalfunds FROM startupdata LIMIT 15;")
+        query_results_b = curb.fetchall()
+
+    startups = []
+    totalfunds = []
+    for result in query_results_a:
+        startups.append(dict(company=result[0], sentiment=result[1]))
+
+    for result in query_results_b:
+        totalfunds.append(result[0])
+        
+    return render_template('startups.html', startups=startups, totalfunds=totalfunds)
