b612edf51b0f25231f1871692cd1d3d472887e81
Andrew Zaldivar <andrew.zaldivar@gmail.com>
2014-09-22 04:41:03 -0700

    Mocked up new front end. Need to hook to SQL later

--- app/views.py
+++ app/views.py
@@ -1,26 +1,18 @@
 from flask import render_template
 from app import app
 import pymysql as mdb
+import json
 
 db = mdb.connect(user="root", host="localhost", db="startupsuccess_db", charset='utf8')
 
 @app.route('/ycstartuppredictor')
 def startups_page_fancy():
     with db:
-        cura = db.cursor()
-        cura.execute("SELECT company, sentiment FROM startupdata LIMIT 15;")
-        query_results_a = cura.fetchall()
-
-        curb = db.cursor()
-        curb.execute("SELECT totalfunds FROM startupdata LIMIT 15;")
-        query_results_b = curb.fetchall()
-
-    startups = []
-    totalfunds = []
-    for result in query_results_a:
-        startups.append(dict(company=result[0], sentiment=result[1]))
-
-    for result in query_results_b:
-        totalfunds.append(result[0])
+        cur_win = db.cursor()
+        cur_win.execute("SELECT company, prediction, probability, totalfunds, investors, founders, titlepoints, topcomment, sentiment, logo FROM fullset LIMIT 15;")
+        query_results_win = cur_win.fetchall()
+    win = []
+    for result in query_results_win:
+        win.append(dict(company=result[0], prediction=result[1], probability=result[2], totalfunds=result[3], investors=result[4], founders=result[5], titlepoints=result[6], topcomment=result[7], sentiment=result[8], logo=result[9]))
         
-    return render_template('startups.html', startups=startups, totalfunds=totalfunds)
+    return render_template('startups.html', win=win)
