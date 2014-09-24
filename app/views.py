0b1934ef1d77a4b19bab51edaacdc94f23db87c9
Andrew Zaldivar <andrew.zaldivar@gmail.com>
2014-09-24 01:41:07 -0700

    Vast improvement on front end

--- app/views.py
+++ app/views.py
@@ -1,18 +1,29 @@
 from flask import render_template
 from app import app
 import pymysql as mdb
-import json
 
 db = mdb.connect(user="root", host="localhost", db="startupsuccess_db", charset='utf8')
 
 @app.route('/ycstartuppredictor')
 def startups_page_fancy():
     with db:
-        cur_win = db.cursor()
-        cur_win.execute("SELECT company, prediction, probability, totalfunds, investors, founders, titlepoints, topcomment, sentiment, logo FROM fullset LIMIT 15;")
-        query_results_win = cur_win.fetchall()
-    win = []
-    for result in query_results_win:
-        win.append(dict(company=result[0], prediction=result[1], probability=result[2], totalfunds=result[3], investors=result[4], founders=result[5], titlepoints=result[6], topcomment=result[7], sentiment=result[8], logo=result[9]))
+        cur_s14 = db.cursor()
+        cur_s14.execute("SELECT company, prediction, probability, totalfunds, investors, founders, titlepoints, sentiment FROM fullset WHERE ycyear = '2014' AND ycsession = 'Summer';")
+        query_results_s14 = cur_s14.fetchall()
+
+        cur_w14 = db.cursor()
+        cur_w14.execute("SELECT company, prediction, probability FROM fullset WHERE ycyear = '2014' AND ycsession = 'Winter';")
+        query_results_w14 = cur_w14.fetchall()
         
-    return render_template('startups.html', win=win)
+    session_s14 = []
+    session_w14 = []
+    
+    for result in query_results_s14:
+        session_s14.append(dict(company=result[0], prediction=result[1],  likelihood=result[2], 
+            totalfunds=result[3], investors=result[4], founders=result[5], 
+            upvotes=result[6], sentiment=result[7]))
+        
+    for result in query_results_w14:
+        session_w14.append(dict(company=result[0], prediction=result[1], likelihood=result[2]))
+        
+    return render_template('startups2.html', session_s14=session_s14, session_w14=session_w14)
