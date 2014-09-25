6250dfe7daccde2b1a9b44ac4d8c12edd34f6895
Andrew Zaldivar <andrew.zaldivar@gmail.com>
2014-09-24 18:46:24 -0700

    Added input form; still figuring out Flask end

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from flask import request
 import pymysql as mdb
 
 db = mdb.connect(user="root", host="localhost", db="startupsuccess_db", charset='utf8')
@@ -39,7 +40,13 @@ def startups_page_fancy():
         cur_w11.execute("SELECT company, prediction, probability, totalfunds, investors, founders, titlepoints, sentiment, website FROM fullset WHERE ycyear = '2011' AND ycsession = 'Winter';")
         query_results_w11 = cur_w11.fetchall()
 
+        cur_s10 = db.cursor()
+        cur_s10.execute("SELECT company, prediction, probability, totalfunds, investors, founders, titlepoints, sentiment, website FROM fullset WHERE ycyear = '2010' AND ycsession = 'Summer';")
+        query_results_s10 = cur_s10.fetchall()
 
+        cur_w10 = db.cursor()
+        cur_w10.execute("SELECT company, prediction, probability, totalfunds, investors, founders, titlepoints, sentiment, website FROM fullset WHERE ycyear = '2010' AND ycsession = 'Winter';")
+        query_results_w10 = cur_w10.fetchall()
         
     session_s14 = []
     session_w14 = []
@@ -53,7 +60,9 @@ def startups_page_fancy():
     session_s11 = []
     session_w11 = []
 
-    
+    session_s10 = []
+    session_w10 = []
+ 
     for result in query_results_s14:
         session_s14.append(dict(company=result[0], prediction=result[1],  likelihood=result[2], 
             totalfunds=result[3], investors=result[4], founders=result[5], 
@@ -94,7 +103,36 @@ def startups_page_fancy():
             totalfunds=result[3], investors=result[4], founders=result[5], 
             upvotes=result[6], sentiment=result[7], url=result[8]))
 
+    for result in query_results_s10:
+        session_s10.append(dict(company=result[0], prediction=result[1],  likelihood=result[2], 
+            totalfunds=result[3], investors=result[4], founders=result[5], 
+            upvotes=result[6], sentiment=result[7], url=result[8]))
+        
+    for result in query_results_w10:
+        session_w10.append(dict(company=result[0], prediction=result[1],  likelihood=result[2], 
+            totalfunds=result[3], investors=result[4], founders=result[5], 
+            upvotes=result[6], sentiment=result[7], url=result[8]))
+
+
     return render_template('startups2.html', session_s14=session_s14, session_w14=session_w14,
             session_s13=session_s13, session_w13=session_w13,
             session_s12=session_s12, session_w12=session_w12,
-            session_s11=session_s11, session_w11=session_w11)
+            session_s11=session_s11, session_w11=session_w11,
+            session_s10=session_s10, session_w10=session_w10)
+
+@app.route('/ycstartuppredictor', methods=['GET'])
+def my_form_post():
+    startup = request.form['startupcompany']
+    
+    with db:
+        cur_startup = db.cursor()
+        cur_startup.execute("SELECT company, prediction, probability, totalfunds, investors, founders, titlepoints, sentiment, website FROM fullset WHERE company = " + startup + ";")
+        query_results_startup = cur_startup.fetchall()
+
+    search_results = []
+    
+    search_results.append(dict(company=query_results_startup[0], prediction=query_results_startup[1],  likelihood=query_results_startup[2], 
+            totalfunds=query_results_startup[3], investors=query_results_startup[4], founders=query_results_startup[5], 
+            upvotes=query_results_startup[6], sentiment=query_results_startup[7], url=query_results_startup[8]))
+
+    return render_template('startups2.html',search_results=search_results)
